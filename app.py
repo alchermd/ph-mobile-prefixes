1f4b571d64a89dcf8f700d36818076219242ecf4
alchermd <johnalcherdoloiras@gmail.com>
2017-07-29 17:04:26 +0800

    Transform previous list into format used by the app.

--- app.py
+++ app.py
@@ -4,17 +4,39 @@ import urllib.request
 
 
 def main():
+    # For debugging
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
+
+    # Setup
     url = 'http://typist.ph/5620/mobile-number-prefixes-2017/'
     sauce = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(sauce, 'lxml')
 
+    # Transform soup into list of dicts.
     all_data = [data.string for data in soup.find_all('td')[2:]]
-    pretty_data = []
-
+    key_val_data = []
     for i in range(0, len(all_data), 2):
-        pretty_data.append({all_data[i]: all_data[i + 1]})
+        key_val_data.append({
+            'number': all_data[i],
+            'network': all_data[i + 1],
+        })
 
-    print(pretty_data)
+    # Transform previous list into format used by the app.
+    reduced_data = []
+    for num in key_val_data:
+        # Check if num.network is in reduced_data
+        if num['network'] in [d['network'] for d in reduced_data]:
+            list(filter(lambda d: d['network'] == num['network'],
+                        reduced_data))[0]['numbers'].append(num['number'])
+        else:
+            reduced_data.append({
+                'network': num['network'],
+                'numbers': [num['number']]
+            })
+
+    pp.pprint(key_val_data)
+    pp.pprint(reduced_data)
 
 
 if __name__ == "__main__":
