0e1a323c77dc36ad96786ea8dcaaa267d5fda336
alchermd <johnalcherdoloiras@gmail.com>
2017-07-29 16:13:38 +0800

    Transformed soup into list of dictionaries.

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ def main():
     sauce = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(sauce, 'lxml')
 
-    print(soup)
+    all_data = [data.string for data in soup.find_all('td')[2:]]
+    pretty_data = []
+
+    for i in range(0, len(all_data), 2):
+        pretty_data.append({all_data[i]: all_data[i + 1]})
+
+    print(pretty_data)
 
 
 if __name__ == "__main__":
